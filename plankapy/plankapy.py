2429d7efdfa119f2d60ca721dd37ccbece0f8dc6
Hayden Welch <hwelch@finelines-engineering.com>
2024-11-18 21:27:18 -0500

    temporary commit for testing routes and background changing

--- plankapy/plankapy.py
+++ plankapy/plankapy.py
@@ -1,31 +1,34 @@
+import sys
+sys.path.append('..')
+
 from utils.routes import Routes, Route
 from utils.models import *
 from utils.handlers import create_session
-
-from typing import Generator
+from utils.constants import *
 
 # This implementation is current;y prettly slow, about 2s for reading all boards and lists in an instance
 class Planka:
-    def __init__(self, url: str, *, username_or_email: str=None, password: str=None, token: str=None):
-        if not url:
-            raise ValueError('A URL must be provided')
-        
+    def __init__(self, url: str, *, username_or_email: str=None, password: str=None, token: str=None):        
         if token:
             self.handler = create_session(url, token=token)
         elif username_or_email and password:
             self.handler = create_session(url, emailOrUsername=username_or_email, password=password)
         else:
-            raise ValueError('Either a token or username/email and password must be provided') 
+            raise ValueError('Either a token or username/email and password must be provided')
         self.routes = Routes(self.handler)
-                
-    def projects(self) -> Generator[Project, None, None]:
-        yield from (Project.from_dict(project) for project in self.routes.projects_index()()['items'])
-    
-    def boards(self, project: Project) -> Generator[Board, None, None]:
-        yield from (Board.from_dict(board) for board in self.routes.projects_show(project.id)()['included']['boards'])
-    
-    def lists(self, board: Board) -> Generator[List, None, None]:
-        yield from (List.from_dict(lst) for lst in self.routes.boards_show(board.id)()['included']['lists'])
-    
+
 if __name__ == '__main__':
-    pass
+    import time
+    import random
+    planka = Planka('http://localhost:3000', username_or_email='demo', password='demo')
+    while True:
+        for project in planka.routes.projects_index():
+            project = Project(**project)
+            gradient = random.choice(Gradient.__args__)
+
+            project.background = Background(gradient).__dict__
+            project.name = gradient
+            changed_background = planka.routes.projects_update(project.id)(**dict(project))
+            changed_background = Project(**changed_background['item'])
+            print(f'{changed_background.name} changed to {changed_background.background}')
+        time.sleep(1)
