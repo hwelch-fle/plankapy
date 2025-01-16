6f15ebb491cc7aeb922649849a6716461ca2122c
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2025-01-16 09:44:46 -0500

    bump version in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='plankapy',
-    version='1.0.1',
+    version='1.0.2',
     description='Python library for Planka API',
     packages=find_packages(),
     include_package_data=True,
