2d99aae4e1bc4e6801b76c8c3cfe7cbbc2764bce
Hayden Welch <hwelch@finelines-engineering.com>
2025-01-18 20:22:02 -0500

    add auth classes

--- plankapy/utils/handlers.py
+++ plankapy/utils/handlers.py
@@ -122,19 +122,39 @@ class JSONHandler(BaseHandler):
     def delete(self) -> JSONResponse:
         return decode_data(super().delete())
 
-def create_session(url: str, *,
-                   emailOrUsername: Optional[str]=None, 
-                   password: Optional[str], 
-                   token: Optional[str]=None) -> JSONHandler:
-    headers: dict[str, str] = {}
-    headers['Content-Type'] = 'application/json'
-    if not token:
-        data = {
-            'emailOrUsername': emailOrUsername,
+
+class BaseAuth:
+    endpoint = None
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name___}: "
+    def authenticate(self) -> str:
+        raise NotImplementedError(f"Authentication not implemeted by {self.__class__.__name__}")
+
+class PasswordAuth(BaseAuth):
+    endpoint = 'api/access-tokens'
+
+    def __init__(self, username_or_email: str, password: str) -> None:
+        self.token = None
+        self.data = {
+            'emailOrUsername': username_or_email,
             'password': password
         }
-        with JSONHandler(url, headers=headers).endpoint_as('api/access-tokens') as handler:
-            response = handler.post(data)
-            token = response['item']
-    headers['Authorization'] = f'Bearer {token}'
-    return JSONHandler(url, headers=headers)
+
+    def authenticate(self, url: str) -> str:
+        with JSONHandler(url, headers={'Content-Type': 'application/json'}).endpoint_as(self.endpoint) as handler:
+            self.token = handler.post(self.data)['item']
+        return f"Bearer {self.token}"
+
+class TokenAuth(BaseAuth):
+    endpoint = 'api/access-tokens'
+
+    def __init__(self, token: str) -> None:
+        self.token = token
+
+    def authenticate(self, url: str) -> str:
+        return f"Bearer {self.token}"
+
+# TODO: Implement SSO auth for httpOnlyToken auth
+# https://github.com/hwelch-fle/plankapy/pull/11/commits/72b8d06208dc961537ef2bcd8d65c11879b8d6b9#
+class HTTPOnlyAuth(BaseAuth): ...
+
