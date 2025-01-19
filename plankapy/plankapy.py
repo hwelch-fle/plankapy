70e1b72399bceba9b3c3949df4fdd6fe35f98d89
Hayden Welch <hwelch@finelines-engineering.com>
2025-01-19 00:18:47 -0500

    update example

--- plankapy/plankapy.py
+++ plankapy/plankapy.py
@@ -55,9 +55,30 @@ class Planka:
         return handler
 
 if __name__ == '__main__':
-    from pprint import pprint
     auth = PasswordAuth(username_or_email='demo', password='demo')
     planka = Planka('http://localhost:3000', auth=auth)
 
-    get_index = planka.routes.get_project_index()
-    pprint(get_index())
+    get_projects = planka.routes.get_project_index()
+
+    for project in get_projects:
+        project = Project(**project)
+        print(project.name)
+        get_current_project = planka.routes.get_project(id=project.id)
+        included = get_current_project()['included']
+
+        if len(included['boards']) == 0:
+            post_board = planka.routes.post_board(projectId=project.id)
+            for board_name in ('Board 1', 'Board 2', 'Board 3'):
+                board = Board(name=board_name,
+                          position=set_position(1),
+                          projectId=project.id)
+                post_board(**board)
+        
+        # Update included after adding new boards
+        included = get_current_project()['included']
+
+        # TODO: Build the 'included' key into the model so
+        # a nested response can be unrolled into correct Model types
+        for board in included['boards']:
+            board = Board(**board)
+            print('\t'+board.name)
