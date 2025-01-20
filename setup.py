f6fd9b66808d26c35b171411a35ab8b19a12c4dd
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2025-01-20 11:17:12 -0500

    urllib is standard, update version, update lock, update pyproject

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='plankapy',
-    version='1.0.1',
+    version='2.0.0',
     description='Python library for Planka API',
     packages=find_packages(),
-    include_package_data=True,
-    package_data={
-        'plankapy': ['config/templates.json'],  # Include templates.json
-    },
 )
