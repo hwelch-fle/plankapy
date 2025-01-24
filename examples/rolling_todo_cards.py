22c839cb3db55103c0530de1679e81920b99feb6
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2025-01-24 13:49:56 -0500

    implicit None return and walrus assignment for object search

--- examples/rolling_todo_cards.py
+++ examples/rolling_todo_cards.py
@@ -3,32 +3,29 @@
 
 import plankapy as ppy
 from plankapy import Planka, PasswordAuth
+from typing import Optional
 
 from datetime import datetime, timedelta, date
 
-def get_project_by_name(planka: Planka, name: str):
+def get_project_by_name(planka: Planka, name: str) -> Optional[ppy.Project]:
     for project in planka.projects:
         if project.name == name:
             return project
-    return None
 
-def get_board_by_date(project: ppy.Project, date: date):
+def get_board_by_date(project: ppy.Project, date: date) -> Optional[ppy.Board]:
     for board in project.boards:
         if board.name == f"{date.year}":
             return board
-    return None
 
-def get_card_by_date(list: ppy.List, date: date):
+def get_card_by_date(list: ppy.List, date: date) -> Optional[ppy.Card]:
     for card in list.cards:
         if card.name == f"Todo {date.day}/{date.month}/{date.year}":
             return card
-    return None
 
-def get_list_by_date(board: ppy.Board, date: date):
+def get_list_by_date(board: ppy.Board, date: date) -> Optional[ppy.List]:
     for list in board.lists:
         if list.name == f"{date.strftime('%B')} {date.year}":
             return list
-    return None
 
 def main():
     
@@ -41,41 +38,34 @@ def main():
     today = datetime.now().date()
     yesterday = today - timedelta(days=1)
     
-    project = get_project_by_name(planka, "Daily Tasks")
-    if not project:
+    if not (project := get_project_by_name(planka, "Daily Tasks")):
         print("Project not found")
         return
 
-    yesterday_board = get_board_by_date(project, yesterday)
-    if not yesterday_board:
+    if not (yesterday_board := get_board_by_date(project, yesterday)):
         print("Yesterday's Board not found")
         return
     
-    yesterday_list = get_list_by_date(yesterday_board, yesterday)
-    if not yesterday_list:
+    if not (yesterday_list := get_list_by_date(yesterday_board, yesterday)):
         print("Yesterday's List not found")
         return
     
-    yesterday_card = get_card_by_date(yesterday_list, yesterday)
-    if not yesterday_card:
+    if not (yesterday_card := get_card_by_date(yesterday_list, yesterday)):
         if len(yesterday_list.cards) > 0 and default_to_last_card:
             yesterday_card = yesterday_list.cards[0]
         else:
             print("Yesterday's Card not found")
             return
     
-    today_board = get_board_by_date(project, today)
-    if not today_board:
+    if not (today_board := get_board_by_date(project, today)):
         today_board = project.create_board(f"{today.year}")
         print(f"Created board for {today.year}")
     
-    today_list = get_list_by_date(today_board, today)
-    if not today_list:
+    if not (today_list := get_list_by_date(today_board, today)):
         today_list = today_board.create_list(f"{today.strftime('%B')} {today.year}")
         print(f"Created List for {today.strftime('%B')} {today.year}")
         
-    today_card = get_card_by_date(today_list, today)
-    if not today_card:
+    if not (today_card := get_card_by_date(today_list, today)):
         print(f"Creating Card for {today.strftime('%d/%m/%Y')}")
         today_card = yesterday_card.duplicate()
         today_card.move(today_list)
