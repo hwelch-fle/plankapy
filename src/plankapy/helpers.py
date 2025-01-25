7b807ba1363f09b368aef88df763fa10b5106b60
Hayden Welch <hwelch@finelines-engineering.com>
2025-01-25 13:00:51 -0500

    remove unused imports

--- src/plankapy/helpers.py
+++ src/plankapy/helpers.py
@@ -1,41 +1,19 @@
-from typing import Optional
-
 from .interfaces import (
     Planka,
     Project,
     User,
-    Notification,
     Board,
     Label,
     Action,
-    Archive,
-    Attachment,
     BoardMembership,
     Card,
     CardLabel,
     CardMembership,
-    CardSubscription,
-    IdentityUserProvider,
     List,
-    ProjectManager,
-    Task,
 )
 
-from .handlers import (
-    BaseAuth,
-    PasswordAuth,
-    TokenAuth,
-)
 
 from .constants import (
-    LabelColor,
-    Gradient,
-    ListSorts,
-    SortOption,
-    Background,
-    BackgroundImage,
-    BoardRole,
-    OFFSET,
     ActionType,
 )
 
