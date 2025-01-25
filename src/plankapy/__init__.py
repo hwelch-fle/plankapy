b7b1c751f0558a72c9a6e40d14a85f99c48ad122
Hayden Welch <hwelch@finelines-engineering.com>
2025-01-25 01:49:49 -0500

    add todo

--- src/plankapy/__init__.py
+++ src/plankapy/__init__.py
@@ -1,5 +1,15 @@
 """Pythonic wrapper for Plankanban API"""
 
+# TODO: Audit codebase and try to remove superfluous new feature imports
+# To allow for plankapy to run on python<=3.11, ideally target >=3.8 (?)
+
+# TODO: Package into a docker container and deploy on release to GHCR
+# This will allow installation of a valid Plankapy container alongside
+# A planka container meaning all requests will be running on loopback/localhost
+
+# TODO: See if plankapy could be shipped in the official Planka container
+
+
 __version__ = "2.0.1"
 
 from .interfaces import (
