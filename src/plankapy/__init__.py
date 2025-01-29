62bc92bfaf8f6b424a9c0d0e48dd873cee9238ce
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2025-01-29 16:31:44 -0500

    bump version

--- src/plankapy/__init__.py
+++ src/plankapy/__init__.py
@@ -1,16 +1,6 @@
 """Pythonic wrapper for Plankanban API"""
 
-# TODO: Audit codebase and try to remove superfluous new feature imports
-# To allow for plankapy to run on python<=3.11, ideally target >=3.8 (?)
-
-# TODO: Package into a docker container and deploy on release to GHCR
-# This will allow installation of a valid Plankapy container alongside
-# A planka container meaning all requests will be running on loopback/localhost
-
-# TODO: See if plankapy could be shipped in the official Planka container
-
-
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 
 # flake8: noqa 
 # imports elevate these objects to the root module
