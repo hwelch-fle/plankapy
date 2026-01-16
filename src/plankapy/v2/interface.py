f53cb0fc0b7bb5dba8f2b6a60b36b3b4d4650ef5
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2026-01-16 15:11:04 -0500

    add basic auth to public Planka interface

--- src/plankapy/v2/interface.py
+++ src/plankapy/v2/interface.py
@@ -2,4 +2,28 @@ from .api import PlankaEndpoints
 from httpx import Client
 
 class Planka:
-    def __init__
+    def __init__(self, client: Client, lang: str='en_US') -> None:
+        self.client = client
+        self.endpoints = PlankaEndpoints(client)
+        self.lang = lang
+        
+    def logon(self, username: str, password: str, 
+              *, token: str='', 
+              accept_terms: bool=False
+        ):
+        """Authenticate with the planka instance"""
+        if not token:
+            token = self.endpoints.createAccessToken(
+                emailOrUsername=username, 
+                password=password,
+                withHttpOnlyToken=True,
+            )['item']
+        if accept_terms:
+            raise NotImplementedError('Term accepting workflow is not implemented yet')
+            self.endpoints.getTerms(type='general', language=self.lang)
+            self.endpoints.acceptTerms(pendingToken=token, signature=None)
+        self.client.headers['Authorization'] = f'Bearer {token}'
+    
+    def logout(self) -> None:
+        self.endpoints.deleteAccessToken()
+        
