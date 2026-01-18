6f1ae53af5e2da10464ab2f8aba42245aab6e464
hwelch-fle <hwelch@finelines-engineering.com>
2026-01-18 17:20:31 -0500

    implement `BoardMembership`

--- src/plankapy/v2/models.py
+++ src/plankapy/v2/models.py
@@ -502,12 +502,69 @@ class Board(PlankaModel[schemas.Board]):
 
 class BoardMembership(PlankaModel[schemas.BoardMembership]):
     """Python interface for Planka BoardMemberships"""
+    
+    # BoardMembership properties
 
+    @property
+    def project(self) -> Project:
+        """The Project the BoardMembership belongs to"""
+        return Project(self.endpoints.getProject(self.schema['projectId'])['item'], self.endpoints)
+    
+    @property
+    def board(self) -> Board:
+        """The Board the BoardMembership is associated with"""
+        return Board(self.endpoints.getBoard(self.schema['boardId'])['item'], self.endpoints)
+    
+    @property
+    def user(self) -> User:
+        """The User the BoardMembership is associated with"""
+        return User(self.endpoints.getUser(self.schema['userId'])['item'], self.endpoints)
+
+    @property
+    def role(self) -> BoardRole:
+        """Role of the user in the board"""
+        return self.schema['role']
+    @role.setter
+    def role(self, role: BoardRole) -> None:
+        """Set the role of the User in the Board"""
+        self.update(role=role)
+    
+    @property
+    def can_comment(self) -> bool:
+        """Whether the user can comment on cards"""
+        if self.role == 'viewer':
+            return self.schema['canComment']
+        return True
+    @can_comment.setter
+    def can_comment(self, can_comment: bool) -> None:
+        """Set if a viewer User can comment"""
+        if self.role == 'viewer':
+            self.update(canComment=can_comment)
+
+    @property
+    def created_at(self) -> datetime:
+        """When the board membership was created"""
+        return datetime.fromisoformat(self.schema['createdAt'])
+    
+    @property
+    def updated_at(self) -> datetime:
+        """When the board membership was last updated"""
+        return datetime.fromisoformat(self.schema['updatedAt'])
 
     # Special Methods
-    def sync(self): ...
-    def update(self): ...
-    def delete(self): ...
+    def sync(self):
+        """Sync the BoardMembership with the Planka server"""
+        _bms = [bm for bm in self.board.board_memberships if bm == self]
+        if _bms:
+            self.schema = _bms.pop().schema
+
+    def update(self, **kwargs: Unpack[paths.Request_updateBoardMembership]) -> None:
+        """Update the BoardMembership"""
+        self.schema = self.endpoints.updateBoardMembership(self.id, **kwargs)['item']
+
+    def delete(self):
+        """Delete the BoardMebership"""
+        return self.endpoints.deleteBoardMembership(self.id)
 
 
 class Card(PlankaModel[schemas.Card]):
