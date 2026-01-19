074ce14578f55f53e975063a8d290aeb341b68ce
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2026-01-19 11:37:31 -0500

    add `NotificationService`

--- src/plankapy/v2/models.py
+++ src/plankapy/v2/models.py
@@ -34,7 +34,7 @@ __all__ = (
     "Label",
     "List",
     "Notification",
-    "NotificationService", #TODO
+    "NotificationService",
     "Project",
     "ProjectManager",
     "Task",
@@ -1675,14 +1675,58 @@ class Notification(PlankaModel[schemas.Notification]):
         """Update the Notification"""
         self.schema = self.endpoints.updateNotification(self.id, **kwargs)['item']
 
+NotificationServiceFormat = Literal['text', 'markdown', 'html']
+NotificationServiceFormats: tuple[NotificationServiceFormat] = NotificationServiceFormat.__args__
   
 class NotificationService(PlankaModel[schemas.NotificationService]):
     """Python interface for Planka NotificationServices"""
+        
+    # NotificationService props
+    
+    @property
+    def user(self) -> User:
+        """The User the NotificationService is associated with"""
+        return [u for u in self.board.users if self.schema['userId'] == u.id].pop()
+    
+    @property
+    def board(self) -> Board:
+        """The Board the NotificationService is associated with"""
+        return Board(self.endpoints.getBoard(self.schema['boardId'])['item'], self.session)
+
+    @property
+    def url(self) -> str:
+        """URL endpoint for Notifications"""
+        return self.schema['url']
+    
+    @property
+    def format(self) -> NotificationServiceFormat: 
+        """Format for notification messages"""
+        return self.schema['format']
+    
+    @property
+    def created_at(self) -> datetime:
+        """When the NotificationService was created"""
+        return datetime.fromisoformat(self.schema['createdAt'])
+
+    @property
+    def updated_at(self) -> datetime:
+        """When the NotificationService was last updated"""
+        return datetime.fromisoformat(self.schema['updatedAt'])
     
     # Special Methods
-    def sync(self): ...
-    def update(self): ...
-    def delete(self): ...
+    def sync(self):
+        """Sync the NotificationService with the Planka server"""
+        _nss = [ns for ns in self.board.project.notification_services if ns == self]
+        if _nss:
+            self.schema = _nss.pop().schema
+        
+    def update(self, **kwargs: Unpack[paths.Request_updateNotificationService]):
+        """Update the NotificationService"""
+        self.schema = self.endpoints.updateNotificationService(self.id, **kwargs)['item']
+    
+    def delete(self):
+        """Delete the NotificationService"""
+        self.endpoints.deleteNotificationService(self.id)
 
 # Valid Gradients
 BackgroundGradient = Literal[
