19158265edc570e42a475c32067c4a9628ae1cc3
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2026-01-20 10:13:04 -0500

    add Model implementation guidelines

--- src/plankapy/v2/models.py
+++ src/plankapy/v2/models.py
@@ -102,6 +102,42 @@ def get_position(items: Sequence[HasPosition], position: Literal['top', 'bottom'
         return 0
     return max((i.position for i in items), default=0) + OFFSET
 
+############################ Model Format #####################################
+# 1) add_*: For creating an association between a model and another model     #
+#   - Card.add_label(label: Label): ...                                       #
+#                                                                             #
+# 2) create_*: For creating a NEW model associated with the current model     #
+#   - Card.create_label(name='new label', color='berry-red', position='top')  #
+#                                                                             #
+# 3) get_*: For associations that require 2+ requests to find                 #
+#   - label.get_cards()                                                       #
+#                                                                             #
+# 4) sync, update, delete: If possible allow GET, PATCH, DELETE passthroughs  #
+#   - If sync required more than 2 requests, don't implement it               # 
+#                                                                             #
+# 4) @property: All attributes and includes that require 1 request            #
+#   - board.labels (in board['included'])                                     #
+#   - board.cards (has endpoint getCards(boardId))                            #
+#                                                                             #
+# 5) @field.setter: Allow setting properties available in Model patch request #
+#   - These setters should implement the update() method of the model         #
+#                                                                             #
+# 6) datetimes: All ISO 8601 strings should be converted to datetime          #
+#   - Use `timezone` property of session if no timezone in ISO string         #
+#                                                                             #
+# 7) Exceptions: Allow HTTPStatusError to be raised unless a necessary        #
+#   - PermissionError: Use session.current_role and current_id for checks     #
+#   - LookupError: If the resource cannot be found (deleted on server)        #
+#   - ValueError: Reserved for file operations (see Attachment)               #
+#   - Any other Exception needs to be clearly documented                      #
+#                                                                             #
+# 8) property iteration: If a list property needs to be iterated, cache it    #
+#   - _list_cards = list.cards                                                #
+#   - This will prevent multiple requests to list._includes                   #
+#   - If your operation is slow, do not cache to prevent de-syncing           #
+#       - Slow is ~1-2s We want to capture accurate board states              #
+###############################################################################
+
 _S = TypeVar('_S', bound=Mapping[str, Any])
 class PlankaModel(Generic[_S]):
     """Base Planka object interface"""
