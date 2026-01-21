70b8d2d92c986f72f3b4c4d14407cc603de43ceb
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2026-01-21 16:39:19 -0500

    add duck walk to testing

--- tests/v2/v2_testing.py
+++ tests/v2/v2_testing.py
@@ -12,3 +12,13 @@ client = Client(base_url=URL)
 planka = Planka(client)
 planka.logon(USER, PASS)
 prj = planka.projects[0]
+
+cards = prj.boards[0].cards
+from random import choice
+from time import sleep
+while True:
+    card = choice(cards)
+    card.add_attachment(r"https://random-d.uk/api/randomimg?t=1769031485189", cover=True, download_url=True)
+    sleep(1)
+    for attach in card.attachments:
+        attach.delete()
