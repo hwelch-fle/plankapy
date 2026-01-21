bbdec0b2cbb87add44399f81787d364340a8994f
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2026-01-21 11:23:24 -0500

    re-work errors to raise detailed messages from server

--- src/plankapy/v2/api/__init__.py
+++ src/plankapy/v2/api/__init__.py
@@ -3,6 +3,6 @@
 from .schemas import *
 from .paths import *
 from .async_paths import *
-from .errors import * # Currently Unused
+from .errors import *
 
 __version__ = "2.0.0"
--- src/plankapy/v2/api/errors.py
+++ src/plankapy/v2/api/errors.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
+from typing import Any
 from httpx import HTTPStatusError
 
 __all__ = (
     "PlankaError",
+    "ERRORS",
     "Conflict",
     "Forbidden",
     "NotFound",
@@ -11,7 +13,14 @@ __all__ = (
     "ValidationError",
 )
 
-class PlankaError(HTTPStatusError): ...
+class PlankaError(HTTPStatusError):
+    def __init__(self, parent: HTTPStatusError, *args: Any, **kwargs: Any) -> None:
+        response_json: dict[str, str] = parent.response.json()
+        message = response_json.get('message', 'NO_MESSAGE')
+        super().__init__(message, request=parent.request, response=parent.response)
+        for problem in response_json.get('problems', []):
+            self.add_note(problem)
+
 
 class Conflict(PlankaError): ...
 """Request conflicts with current state of the resource"""
@@ -30,3 +39,12 @@ class UnprocessableEntity(PlankaError): ...
 
 class ValidationError(PlankaError): ...
 """Request validation failed due to missing or invalid parameters"""
+
+ERRORS: dict[str, type[PlankaError]] = {
+    'E_CONFLICT': Conflict,
+    'E_FORBIDDEN': Forbidden,
+    'E_NOT_FOUND': NotFound,
+    'E_UNAUTHORIZED': Unauthorized,
+    'E_UNPROCESSABLE_ENTITY': UnprocessableEntity,
+    'E_MISSING_OR_INVALID_PARAMS': ValidationError,
+}
