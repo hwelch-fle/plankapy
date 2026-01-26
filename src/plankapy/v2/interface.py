298f3e0eb6c9e1de2d9363d2755f43030c1380f7
hwelch-fle <hwelch@finelines-engineering.com>
2026-01-26 01:04:43 -0500

    add doc and move lanf to terms acceptance

--- src/plankapy/v2/interface.py
+++ src/plankapy/v2/interface.py
@@ -1,11 +1,50 @@
 """
 Base interface for Planka
+
+When reading the documentation, all returned `list[PlankaModel]` types are 
+actually `models.ModelLists` at runtime. These will also be properly type hinted by 
+your static type checker and expose some alternate ways of indexing into model lists:
+
+Example:
+```python
+>>> type(planka.projects)
+ModelList
+
+# Using an exact schema filter
+>>> planka.projects[{'name': 'My Project'}]
+[Project(name='My Project', ...)]
+
+# Using a functional schema filter (name is replaced with the value of name)
+>>> planka.projects[{'name': lambda name: 'My' in name}]
+[Project(name='My Project', ...), Project(name='My Other Project', ...)]
+
+# Using a model filter
+>>> planka.projects[lambda project: project.owner == planka.me]
+[<previous results>..., Project('Another Project I Own', ...)]
+```
+
+All dictionary based schema filtering can be done per key and the model will only 
+be included in the output if all filters match.
+
+Additionally, a `dpop` method is included that allows popping from a model list with 
+a default value if no results
+```python
+>>> filtered = to_do_list.cards[lambda c: c.due_date and (c.due_date - datetime.now()).days < 4]
+>>> if next_card := filtered.dpop():
+...     print(next_card.url)
+...     print(next_card.name)
+... else:
+...     print('No results')
+https://planka.mydomain.com/cards/1234...
+'Critical Project'
+--or--
+No results
+```
 """
 
 from __future__ import annotations
 from functools import cached_property
 from typing import Sequence
-from warnings import warn
 from datetime import timezone
 
 from httpx import Client, HTTPStatusError
@@ -25,22 +64,98 @@ DEFAULT_LANG = os.environ.get('PLANKA_LANG', 'en-US')
 del os
 
 class Planka:
-    def __init__(self, client: Client, *, lang: str=DEFAULT_LANG, timezone: timezone = timezone.utc) -> None:
-        self.client = client
-        self.endpoints = PlankaEndpoints(client)
-        if lang not in Languages:
-            warn(f'{lang} is not currently supported by Planka, using {DEFAULT_LANG}', EncodingWarning)
-            lang = DEFAULT_LANG
-        self.lang = lang
+    """Root object for connecting to a Planka instance
+    
+    A Planka instance can be initialized using a `base_url` or a `httpx.Client` objcet.
+    If both arguments are passed, the `base_url` of the passed client will be overriden with
+    the url passed to the `base_url` argument
+
+    Example:
+
+        Using the default Client with a base_url
+        ```python
+        >>> planka = Planka('https://planka.example.com')
+        ... planka.client
+        <httpx.Client object at 0x...>
+
+        >>> planka.client.base_url
+        URL('https://planka.example.com')
+        ```
+        Client based initialization
+        ```python
+        >>> hooks = {'request': [hook1, hook2], 'response': [hook3, hook4]}
+        ... client = Client(event_hooks=hooks)
+        ... planka = Planka('https://planka.example.com', client=client)
+        ... planka.client.event_hooks
+        {'request': [<hook1>, <hook2>], 'response': [<hook3>, <hook4>]}
+        ```
+    
+    All Planka sessions will respect the configuration of the passed client object. For more configuration 
+    options, see the httpx docs: https://www.python-httpx.org/advanced/clients/
+
+    After initializing, the Planka instance required the authorization flow to 
+    take place using the `login` method
+
+    Example:
+        ```python
+        >>> # Using an API key
+        ... planka.login(api_key='<my_api_key>')
+        ... planka.projects
+        [Project(...), Project(...), ...]
+        ```
+        ```python
+        >>> # First time login (accept instance ToS)
+        ... planka.login(username='me@email.com', password='mypassword', accept_terms=True)
+        TERMS OF SERVICE
+        --- More ToS ---
+
+        >>> planka.projects
+        [Project(...), Project(...), ...]
+        ```
+
+    Logging in with a username and password will work, but it's best practice to authenticate 
+    using an api key, which you can get from your instance administrator.
+    """
+
+    def __init__(self, 
+                 base_url: str|None=None, 
+                 *, 
+                 client: Client|None=None, 
+                 timezone: timezone = timezone.utc) -> None:
+        """
+        Args:
+            base_url: The base url of the instance (e.g. `https://planka.app`)
+            client: An optional pre-configured `httpx.Client` object to use as the session client
+            timezone: An optional timezone to cast all datetimes to for date math
+
+        Raises:
+            ValueError: If no base_url or no client with a base_url
+        """
+        if client is None and base_url:
+            self.client = Client(base_url=base_url)
+        elif client and base_url is None:
+            if not client.base_url:
+                raise ValueError(
+                    f'If using a client, the client\'s base_url attribute must be set '
+                    'or a base_url must be passed to the Planka initializer'
+                )
+            self.client = client
+        elif base_url and client:
+            self.client = client
+            self.client.base_url = base_url
+        else:
+            raise ValueError(f'base_url and/or client must be passed')
+        
+        self.endpoints = PlankaEndpoints(self.client)
         self.timezone = timezone
         
-        # Assigned after logon() is called
+        # Assigned after login() is called
         self.current_role: UserRole | None = None
         self.current_id = None
     
-    def accept_terms(self, pending_token: str, terms_type: TermsType='general'):
+    def accept_terms(self, pending_token: str, terms_type: TermsType='general', lang: Language='en-US'):
         """If the User has never logged on, or is required to accept new terms, allow them to do so"""
-        terms = self.endpoints.getTerms(type=terms_type, language=self.lang)['item']
+        terms = self.endpoints.getTerms(type=terms_type, language=lang)['item']
         print(terms['content'])
         sig = terms['signature']
         self.endpoints.acceptTerms(pendingToken=pending_token, signature=sig)
@@ -50,7 +165,8 @@ class Planka:
               username: str|None=None, 
               password: str|None=None, 
               api_key: str|None=None, 
-              accept_terms: TermsType | None=None) -> None:
+              accept_terms: TermsType | None=None,
+              terms_lang: Language='en-US') -> None:
         
         """Authenticate with the planka instance
         
@@ -59,6 +175,7 @@ class Planka:
             password (str | None): User password
             api_key (str | None): User API Key
             accept_terms (TermsType | None): If you user has not accepted the terms, run the term acceptance flow
+            terms_lang: If accepting terms, request them in this language
             
         Note:
             After accepting the terms, please get an API key from the Planka server. If you need to accept extended terms, please 
@@ -77,8 +194,8 @@ class Planka:
             except HTTPStatusError as e:
                 if accept_terms is None:
                     raise PermissionError(f'Please logon again with `accept_terms` set to the terms you must accept')
-                self.accept_terms(e.response.json()['pendingToken'], terms_type=accept_terms)
-                self.logon(username=username, password=password)
+                self.accept_terms(e.response.json()['pendingToken'], terms_type=accept_terms, lang=terms_lang)
+                self.login(username=username, password=password)
         
         # Invalid Creds
         else:
