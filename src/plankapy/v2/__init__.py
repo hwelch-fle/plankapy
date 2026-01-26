b6a691db9ed606ca34726191de768668afb26b37
hwelch-fle <hwelch@finelines-engineering.com>
2026-01-26 16:02:36 -0500

    expose utils

--- src/plankapy/v2/__init__.py
+++ src/plankapy/v2/__init__.py
@@ -1,15 +1,19 @@
-__all__ = ('Planka', 'models', 'api')
+__all__ = ('Planka', )
 
 from .interface import Planka as Planka
 from .interface import Client as Client
 from . models import *
 from . import (
-    api, 
-    models,
+    api as api, 
+    models as models,
+    utils as utils,
 )
 from .models._helpers import (
     POSITION_GAP as POSITION_GAP,
     model_list as model_list,
     ModelList as ModelList,
+)
+from .utils import (
+    board_to_csv as board_to_csv,
     due_in as due_in,
 )
