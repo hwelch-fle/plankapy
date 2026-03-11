0432bfa5ad14aca6d17439163ea3136580b365a6
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2026-03-11 17:18:01 -0400

    feat: Add `create_api_key` method and update `terms_type`

--- src/plankapy/v2/models/user.py
+++ src/plankapy/v2/models/user.py
@@ -184,9 +184,9 @@ class User(PlankaModel[schemas.User]):
         self.update(defaultProjectsOrder=default_projects_order)
     
     @property
-    def terms_type(self) -> TermsType:
+    def terms_type(self) -> TermsType | None:
         """Type of terms applicable to the user based on role"""
-        return self.schema['termsType']
+        return self.schema.get('termsType')
     
     @property
     def is_sso_user(self) -> bool:
@@ -435,4 +435,25 @@ class User(PlankaModel[schemas.User]):
         if notification_service in self.notification_services:
             notification_service.delete()
     
+    def create_api_key(self, overwrite: bool = False) -> str | None:
+        """Generate an API key for the user (must be admin or logged in user)
+        
+        Args:
+            overwrite: If the user already has an API Key, this will overwrite it with a new one and return the new key
+            
+        Returns:
+            A new API Key for the user (This is only sent once! so make sure to save it somewhere)
+            
+        Raises:
+            PermissionError if the user already has a key and `overwrite` is unset
+        """
+        if self.schema.get('apiKeyPrefix'):
+            if overwrite:
+                self.update(apiKey=None)
+            else:
+                raise PermissionError('User already has an API Key, run again with `overwrite` enabled to create a new one')
+        else:
+            return self.endpoints.createUserApiKey(self.id)['included']['apiKey']
+    
 from .notification_service import NotificationService
+from . import Card, Board
